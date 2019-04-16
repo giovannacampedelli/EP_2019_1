8b3ca5ae00fab3df1306d3678ddcecaac4a5fe3c
Your Name <you@example.com>
2019-04-16 10:16:48 -0300

    mudei os nomes

--- ep1.py
+++ ep1.py
@@ -1,8 +1,8 @@
 # EP 2019-1: Escape Insper
 #
 # Alunos: 
-# - aluno A: Fulano da Silva, fulanos@insper.edu.br
-# - aluno B: Sicrano de Almeida, sicranoa1@insper.edu.br
+# - aluno A: Caroline Chaim, carolineclc@al.insper.edu.br
+# - aluno B: Giovanna Campedelli, giovannahc@al.insper.edu.br
 
 def carregar_cenarios():
     cenarios = {
